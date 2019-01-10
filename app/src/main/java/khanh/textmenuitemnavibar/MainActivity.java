ef29e357101dfa98d20568017a85807a9499ad42
QUOC-KHANH\saakh <saakhaka@gmail.com>
2019-01-10 16:13:30 +0700

    Text Log D

--- app/src/main/java/khanh/textmenuitemnavibar/MainActivity.java
+++ app/src/main/java/khanh/textmenuitemnavibar/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("ssss", "ssss");
 
     }
 
